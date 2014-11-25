c3a08074e0a22e7bb5fc074057e7a8090e0c7b8a
Saumitra Aditya <saumitraaditya@users.noreply.github.com>
2014-11-24 21:09:10 -0500

    Create movie_data.java

---
+++ movie_data.java
@@ -0,0 +1,16 @@
+public class movie_data
+   {
+	   String movie_name;
+	   int twitter_score;
+	   int RT_audience_cmt;
+	   int RT_critic_cmt;
+	   int RT_audience_score;
+	   int RT_critic_score;
+	   int YT_cmt_score;
+	   int YT_views;
+	   int YT_likes;
+	   int YT_dislikes;
+	   long collections;
+	   
+	   
+   }
